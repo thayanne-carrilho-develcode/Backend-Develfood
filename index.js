d53133e7a3388ceaab42669c1cf6c4aa65e9d7f2
Develcode <thayanne.carrilho@develcode.com.br>
2025-02-13 17:51:24 -0300

    back2

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
+const jsonServer = require('json-server');
+const cors = require('cors');
 
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(cors()) 
-server.use(middlewares)
-server.use(jsonServer.bodyParser)
+server.use(cors({
+  origin: 'https://develfood-thay.onrender.com'
+}));
 
-server.use('/api', router)
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
 
-const PORT = process.env.PORT || 3001
+server.use(router); // Remove o '/api', mantendo as rotas diretas
+
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-  console.log(`JSON Server rodando na porta ${PORT}`)
-})
+  console.log(`JSON Server rodando na porta ${PORT}`);
+});
