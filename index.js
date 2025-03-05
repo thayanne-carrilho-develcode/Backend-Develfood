a180224285288e3af439081319d415ec73420354
Develcode <thayanne.carrilho@develcode.com.br>
2025-03-05 10:22:33 -0300

    add-new-deploy

--- index.js
+++ index.js
@@ -5,14 +5,24 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+const allowedOrigins = [
+  "https://develfood-thay.onrender.com",
+  "https://develfood-thayanne.onrender.com"
+];
+
 server.use(cors({
-  origin: 'https://develfood-thay.onrender.com'
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
 }));
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-
-server.use(router); 
+server.use(router);
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
